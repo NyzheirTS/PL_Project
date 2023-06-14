1d85d8e27016c0a7412b94279b6a78f3f8b16a04
R3VTomato <Nyzheirwarner@gmail.com>
2023-06-14 00:28:07 -0400

    Version 0.0.1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,11 @@
 module com.nyzheirwarner.nyny {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
+    requires java.sql;
+    requires annotations;
+    requires java.prefs;
+    requires java.net.http;
             
                             
     opens com.nyzheirwarner.nyny to javafx.fxml;
